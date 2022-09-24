1d0d90b93be8aca86f0c7717804080547dc26381
Frazier Odhiambo <ofrazy@gmail.com>
2022-09-25 02:17:41 +0300

    booking feature added

---
+++ controllers/bookings.js
@@ -0,0 +1,35 @@
+const Booking = require("../models/Booking");
+
+module.exports = {
+  
+    createBooking: async (req, res) => {
+      try {
+        await Booking.create({
+          kennel: req.params.id,
+          bookedBy: req.user.userName,
+          bookedById: req.user.id,
+          pet: req.body.pet,
+          comment: req.body.comment,
+          BookingFrom: req.body.BookingFrom,
+          BookingTill: req.body.BookingTill,
+        });
+        console.log("New booking has been created");
+        res.redirect("/post/"+req.params.id);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    deleteBooking: async (req, res) => {
+      try {
+        await Booking.deleteOne({
+          _id: req.params.commentid
+        })
+        console.log("Booking has been deleted");
+        res.redirect("/post/"+req.params.postid)
+  
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    
+  };
---
+++ models/Booking.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const BookingSchema = new mongoose.Schema({
+  kennel: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Kennel",
+  },
+  bookedBy: {
+    type: String,
+    ref: "User",
+  },
+  bookedById: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  pet: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  BookingFrom: {
+    type: String,
+    required: true,
+  },
+  BookingTill: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Booking", BookingSchema);
